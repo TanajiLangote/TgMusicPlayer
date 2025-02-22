a3cb91fbe720c78591fdd9fa641f7a5da564de74
Tanaji Langote <102846255+TanajiLangote@users.noreply.github.com>
2025-02-22 09:15:51 +0530

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -14,9 +14,9 @@ from helpers.decorators import errors, authorized_users_only
 @authorized_users_only
 async def pause(_, message: Message):
     if callsmusic.pause(message.chat.id):
-        await message.reply_text(" ⏸ Paused!")
+        await message.reply_text(" ⏸ Paused! \n Join @TeleVcplayer")
     else:
-        await message.reply_text("Nothing is Playing")
+        await message.reply_text("Nothing is Playing \n Join @TeleVcplayer")
 
 
 @Client.on_message(command("resume") & other_filters)
@@ -24,9 +24,9 @@ async def pause(_, message: Message):
 @authorized_users_only
 async def resume(_, message: Message):
     if callsmusic.resume(message.chat.id):
-        await message.reply_text(" ▶️ Resumed ")
+        await message.reply_text(" ▶️ Resumed \n Join @TeleVcplayer")
     else:
-        await message.reply_text("Nothing is Paused")
+        await message.reply_text("Nothing is Paused \n Join @TeleVcplayer")
 
 
 @Client.on_message(command("stop") & other_filters)
@@ -34,7 +34,7 @@ async def resume(_, message: Message):
 @authorized_users_only
 async def stop(_, message: Message):
     if message.chat.id not in callsmusic.active_chats:
-        await message.reply_text("Nothing is Playing")
+        await message.reply_text("Nothing is Playing \n Join @TeleVcplayer")
     else:
         try:
             queues.clear(message.chat.id)
@@ -42,7 +42,7 @@ async def stop(_, message: Message):
             pass
 
         await callsmusic.stop(message.chat.id)
-        await message.reply_text("🗑 Cleared the queue and left the call 🏃")
+        await message.reply_text(" Cleared the queue and left the call \n Join @TeleVcplayer")
 
 
 @Client.on_message(command("skip") & other_filters)
@@ -50,7 +50,7 @@ async def stop(_, message: Message):
 @authorized_users_only
 async def skip(_, message: Message):
     if message.chat.id not in callsmusic.active_chats:
-        await message.reply_text("Nothing is Playing")
+        await message.reply_text("Nothing is Playing \n Join @TeleVcplayer")
     else:
         queues.task_done(message.chat.id)
 
@@ -61,7 +61,7 @@ async def skip(_, message: Message):
                 message.chat.id, queues.get(message.chat.id)["file"]
             )
 
-        await message.reply_text("Skipped!")
+        await message.reply_text("Skipped \n Join @TeleVcplayer")
 
 
 @Client.on_message(command("mute") & other_filters)
@@ -71,11 +71,11 @@ async def mute(_, message: Message):
     result = callsmusic.mute(message.chat.id)
 
     if result == 0:
-        await message.reply_text("🔇 Muted ")
+        await message.reply_text("Ok, Muted  \n please Join @TeleVcplayer ")
     elif result == 1:
-        await message.reply_text("🤐 Already muted!")
+        await message.reply_text(" Already muted! \n Please Join @TeleVcplayer")
     elif result == 2:
-        await message.reply_text("🎙❎ Not in voice chat!")
+        await message.reply_text("🎙 Not in voice chat! \n Please Join @TeleVcplayer")
 
 
 @Client.on_message(command("unmute") & other_filters)
@@ -85,8 +85,8 @@ async def unmute(_, message: Message):
     result = callsmusic.unmute(message.chat.id)
 
     if result == 0:
-        await message.reply_text("🔊 Unmuted!")
+        await message.reply_text("ok, Unmuted! \n PLease Join @TeleVcplayer")
     elif result == 1:
-        await message.reply_text("😆 Already unmuted!")
+        await message.reply_text("Ok, Already unmuted \n Please Join @TeleVcplayer")
     elif result == 2:
-        await message.reply_text("🎙❎ Not in voice chat!")
+        await message.reply_text(" Not in voice chat! \n PLease Join @TeleVcplayer")
