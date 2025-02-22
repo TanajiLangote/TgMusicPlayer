1e8a5ad8f51fc237c597b5116144b8609ce1b738
Tanaji Langote <102846255+TanajiLangote@users.noreply.github.com>
2025-02-22 09:08:29 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -3,28 +3,23 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 from helpers.filters import other_filters2
 
-STICKER = "CAACAgQAAx0CQ2C8OgACsqNhiisoWUQROohUrpaGzDsHsot3dQACVxYAAtqjlSznBlAxygdMwyIE"
+
 
 
 START_TEXT = """
-👋 Hey {} I am an Telegram Groups Music Player, I let you play music in your group's voice chat.
-
-**Commands** [Here](telegra.ph/A-Simple-Group-Music-player-bot-by-SDBotsz-11-09-2)
-
-Join @SDBotsz. 🔥
+Hello {} I am an Telegram Groups Music Player, I let you play music in your group's voice chat.
 """
 
 START_BUTTON = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton('Channel', url="https://t.me/SDBOTs_inifinity"),
-        InlineKeyboardButton('Group', url='https://t.me/SDBOTz')
+        InlineKeyboardButton('Channel', url="https://t.me/TeleVcplayer")
         ]]
   
 )
 
 @Client.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
-    await update.reply_sticker(STICKER)
+   
     await update.reply_text(
         text=START_TEXT.format(update.from_user.mention),
         reply_markup=START_BUTTON,
